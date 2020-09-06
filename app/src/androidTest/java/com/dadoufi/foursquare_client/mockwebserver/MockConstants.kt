60fa6fd2f15fa0fa0687f496ddef5ca0bd94c2af
Konstantinos Tsantoulas <Konstantinos@funda.nl>
2020-09-07 01:02:45 +0200

    Fixed wrong request url in ui test

--- app/src/androidTest/java/com/dadoufi/foursquare_client/mockwebserver/MockConstants.kt
+++ app/src/androidTest/java/com/dadoufi/foursquare_client/mockwebserver/MockConstants.kt
@@ -5,7 +5,7 @@ const val API_URL = "http://localhost:8080/"
 
 object APIPaths {
     const val PIZZA_SEARCH =
-        "http://localhost:8080/venues/search?near=Amsterdam&radius=1000&limit=10&query=pizza&client_id=DWU21SJWIILG5JSWJCRVPYVJQTT4LGGI44BKN3GVSUPTPQGJ&client_secret=525VCSB2ZWO3G0XYODUZP4BHDFBYJ12Q2B5NSNPPRBXFNIS1&v=20120609"
+        "http://localhost:8080/venues/search?near=amsterdam&radius=1000&limit=10&query=pizza&client_id=DWU21SJWIILG5JSWJCRVPYVJQTT4LGGI44BKN3GVSUPTPQGJ&client_secret=525VCSB2ZWO3G0XYODUZP4BHDFBYJ12Q2B5NSNPPRBXFNIS1&v=20120609"
     const val VENUE_DETAILS =
         "http://localhost:8080/venues/4a27db7bf964a52016941fe3?client_id=DWU21SJWIILG5JSWJCRVPYVJQTT4LGGI44BKN3GVSUPTPQGJ&client_secret=525VCSB2ZWO3G0XYODUZP4BHDFBYJ12Q2B5NSNPPRBXFNIS1&v=20120609"
 }
